a348c9803bdcdc93b0b43f81ddcc6b0bbf19d800
kbrien11 <49761400+kbrien11@users.noreply.github.com>
2021-10-11 15:11:51 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './App.css';
-import { FaSearch, FaSpinner, FaSun, FaCloudSunRain, FaCloud } from 'react-icons/fa';
+import { FaSearch, FaSpinner, FaSun, FaCloudSunRain, FaCloud,FaTrash } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
@@ -14,45 +14,21 @@ const App = () => {
   const [inputCity, setInputCity] = useState('');
   const [inputState, setInputState] = useState('');
   const [cities, setCities] = useState([]);
+  const [favoriteCities, setFavoriteCities] = useState([]);
   const [loading, setLoading] = useState('');
   const [isError, setIsError] = useState(false);
   const [noCitiesFound, setNoCitiesFound] = useState(false);
   const [inputShowForm, setShowForm] = useState(false);
+  const [inputShowFavoriteCities, setShowFavoriteCities] = useState(false);
   const [inputUpdateForm, setUpdateForm] = useState(false);
   const [inputCityAddedText, setInputCityAddedTest] = useState(false);
   const[inputCityUpdatedText, setInputCityUpdatedText] = useState(false);
+  const[inputaddToFavoriteText, setaddToFavoriteText] = useState(false);
   const [inputCityExistsError, setCityExistError] = useState(false);
   const [inputCityDeletedText, setCityDeletedText] = useState(false);
   const [token, setToken] = useState(false)
 
-  // const getWeather = async () => {
-  //   try {
-  //     setIsError(false);
-  //     setLoading(true);
-  //     setNoCitiesFound(false);
-  //     setCities([]);
-  //     if (inputWeatherCondition == '') {
-  //       setIsError(true);
-  //       setLoading(false);
-  //       setTimeout(setErrorTimeout, 4000);
-  //     } else {
-  //       const response = await fetch(
-  //         `http://127.0.0.1:5000/api/${inputWeatherCondition}/`
-  //       );
-  //       const res = await response.json();
-  //       console.log(res);
-  //       if (res.data) {
-  //         setCities(res.data);
-  //         setLoading(false);
-  //         setNoCitiesFound(true);
-  //         setTimeout(noCityFoundTimeOut, 6000);
-  //         console.log(res.data);
-  //       }
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  useEffect(() => {getFavoriteCityData()},[])
 
   const deleteCity = async () => {
     const endpoint = `http://localhost:8080/cities/${token}`
@@ -123,6 +99,38 @@ const App = () => {
     }
   }
 
+  const addToFavorites = async () => {
+    setIsError(false)
+    const endpoint = "http://localhost:8080/cities/update"
+    const data = {
+      id:token,
+      city: inputCity,
+      state: inputState,
+      favorite:1
+    }
+
+    const configs = {
+      method: "PUT",
+      mode: "cors",
+      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*", 'Accept': 'application/json' },
+      body: JSON.stringify(data)
+    }
+
+    const response = await fetch(endpoint, configs);
+    console.log(response)
+    const res = response.json
+    if (response.status === 200) {
+      console.log(res)
+      setaddToFavoriteText(true)
+      setTimeout(cityaddedToFavoriteTimeOut, 5000)
+      setInputState("")
+      setInputCity("")
+    }
+    else {
+      setCityExistError(true)
+    }
+  }
+
 
   const sendUpdatedData = async () => {
     setIsError(false)
@@ -161,6 +169,26 @@ console.log(inputCity)
 console.log(inputState)
 
 
+const getFavoriteCityData = async () =>{
+  try{
+    const response = await fetch(
+      "http://localhost:8080/cities/favorite"
+    );
+    const res = await response.json()
+        console.log(res)
+    if (res.length>0){
+      setShowFavoriteCities(true)
+      setFavoriteCities(res)
+    }
+    else{
+      console.log("no foavirte data")
+    }
+  } catch(error) {
+   console.log(error)
+  }
+}
+
+console.log(favoriteCities)
 
   const getWeatherFromCity = async () => {
     try {
@@ -180,6 +208,7 @@ console.log(inputState)
         console.log(response[0])
         console.log(res);
         if (res.length > 0) {
+          setShowFavoriteCities(false)
           setCities(res);
           setToken(res[0])
           setInputState(res[2])
@@ -222,10 +251,14 @@ console.log(inputState)
 
   console.log(typeof (cityObj[2]))
 
-  const data = cities.map((city) => {
+  const favoriteData = favoriteCities.map((city) => {
     return <CityComponent
 
-      data={city}
+      name={city[1]}
+      state ={city[2]}
+      temp = {city[3]}
+      wind = {city[4]}
+      description ={city[5]}
     />
   })
 
@@ -233,6 +266,10 @@ console.log(inputState)
 
   const noCityFoundTimeOut = () => {
     setNoCitiesFound(false);
+  };
+
+  const cityaddedToFavoriteTimeOut = () => {
+    setaddToFavoriteText(false);
   };
 
   const CityUpdatedTimeOut = () => {
@@ -280,6 +317,8 @@ console.log(inputState)
 
   return (
     <div>
+
+    
       <div className="searchBar">
         <input
           type="text"
@@ -309,6 +348,14 @@ console.log(inputState)
           ' '
         )}
       </div>
+
+      {inputShowFavoriteCities &&  <div className = "favoriteCityData">
+ 
+  <p> List of favorite cities ({favoriteData.length}) </p>
+  <hr></hr>
+       { favoriteData} 
+
+</div> }
       {cities.length > 0 && (
         <div className="table-container">
           <div className="home-table">
@@ -323,7 +370,7 @@ console.log(inputState)
 
             <p> If you would like to delete the {inputWeatherCondition} from the DB, please click here <span> <button type="button" onClick={(e) => deleteCity()}>
               {' '}
-              Delete City </button> </span> </p>
+             <FaTrash/> </button> </span> </p>
             {/* <p>
               <BootstrapTable
                 keyField="Name"
@@ -342,6 +389,11 @@ console.log(inputState)
             <button type="button" onClick={(e) => showUpdateForm()}>
               {' '}
               Update {inputWeatherCondition}
+            </button>{' '}
+
+            <button type="button" onClick={(e) => addToFavorites()}>
+              {' '}
+              Add To Favorites
             </button>{' '}
             <div>
               {inputShowForm && (
@@ -402,7 +454,7 @@ console.log(inputState)
         </div>
       )}
 
-
+            {inputaddToFavoriteText && <p> City added to favorites!!</p>}
       {noCitiesFound && cities.length == 0 && (
         <p>
           {' '}
